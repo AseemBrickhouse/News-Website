a4107961b3c32f3948a955678064151c16410faf
AseemBrickhouse <aseembrickhouse55@gmail.com>
2023-06-01 14:55:27 -0500

    Refactor of AccountViews

--- news_website/api/APIUtility.py
+++ news_website/api/APIUtility.py
@@ -44,3 +44,14 @@ def PopularUserArticles(account):
         convertedArticle = ArticleSerializer(article)
         popular_articles[convertedArticle.data['id']] = convertedArticle.data
     return(popular_articles)
+
+
+def get_user_account(token):
+    try:
+        token = Token.objects.get(key=token)
+        print(token)
+        user_account = User.objects.all().filter(id=token.user_id)[0].account
+        return user_account
+    except Token.DoesNotExist:
+         return None
+
