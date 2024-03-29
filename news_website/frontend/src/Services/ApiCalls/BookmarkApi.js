d7e968aa9d0a659f3fa52b24bdd72c080f18084c
AseemBrickhouse <aseembrickhouse55@gmail.com>
2024-03-29 15:57:12 -0500

    Fixed article bookmarking

--- news_website/frontend/src/Services/ApiCalls/BookmarkApi.js
+++ news_website/frontend/src/Services/ApiCalls/BookmarkApi.js
@@ -1,6 +1,6 @@
 import { BASE_URL } from "../../store/baseURLS";
 
-export const handleBookMark = async () => {
+export const handleBookMark = async (article_id, type) => {
   const url = `${BASE_URL}/api/articles/${article_id}/bookmark/`;
   const headers = {
     Accept: "application/json",
